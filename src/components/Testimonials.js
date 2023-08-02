7f4df5dbb80b48aec1059196b444f98c2eba5951
unknown <jayprakashpj2525@gmail.com>
2023-08-02 19:02:12 +0530

    ch

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,43 +1,39 @@
+// export default Testimonials;
 import ParticleLink from "./ParticleLink";
 import Data from "../Data/TestimonialData";
 const Testimonials = () => {
   return (
-    <section className="my-10">
-      <h1 className="text-orange-500 font-bold text-lg lg:text-2xl text-center">
-        WHAT CLIENTS SAY?
-      </h1>
+    <div className="my-8 relative">
+      <ParticleLink />
 
-      <div className=" relative my-5">
-        <ParticleLink />
+      <div className="absolute top-0 w-[100%] h-[100%] flex  flex-col  items-center">
+        <h1 className="text-[#fff] py-14 text-3xl md:text-4xl lg:text-5xl font-semibold">
+          WHAT CLIENTS SAY?
+        </h1>
 
-        <div className="absolute top-0 w-[100%]   flex  flex-col  items-center">
-          <div className="flex justify-start items-center overflow-x-scroll w-[40vw]">
-            {Data.map((_, index) => {
-              return (
-                <div
-                  key={index}
-                  className="flex flex-col justify-center items-center py-10 w-[100vw] space-x-4 px-8 text-center"
-                >
-                  <img className="w-24" src={_.image} alt="img.png" />
-                  <div>
-                    <h1 className="text-white text-2xl font-semibold">
-                      {_.Name}
-                    </h1>
-                    <p className="text-[#d0d0d0] text-xs leading-8 py-4">
-                      {_.p}
-                    </p>
-                    <div className="flex space-x-3">
-                      <p>{_.star}</p>
-                      <p className="text-[#ff4747]">{_.review}</p>
-                    </div>
+        <div className="flex space-x-20 pt-10">
+          {Data.map((_, index) => {
+            return (
+              <div className="flex max-w-lg space-x-4 px-8">
+                <div className="flex items-center p-3 rounded-md border-[#ccc] border-[1px]">
+                  <img className="w-[28rem]" src={_.image} alt="img.png" />
+                </div>
+                <div>
+                  <h1 className="text-white text-2xl font-semibold">
+                    {_.Name}
+                  </h1>
+                  <p className="text-[#d0d0d0] leading-8 py-4">{_.p}</p>
+                  <div className="flex space-x-3">
+                    <p>{_.star}</p>
+                    <p className="text-[#ff4747]">{_.review}</p>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 

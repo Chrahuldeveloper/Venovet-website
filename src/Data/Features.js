69261cd88ba3797e358af0dd6fe4c80dca682958
unknown <jayprakashpj2525@gmail.com>
2023-08-03 19:15:47 +0530

    footer

--- src/Data/Features.js
+++ src/Data/Features.js
@@ -2,42 +2,42 @@ import { LiaAddressCard } from "react-icons/lia";
 
 const Data = [
   {
-    icons: <LiaAddressCard size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
   {
-    icons: <LiaAddressCard  size={30} color="orange"/>,
+    icons: <LiaAddressCard size={30} color="orange" />,
     Tittle: "Trusted Franchise",
     Para: "Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis. Vivamus ac ultrices diam, vitae accumsan tellus.",
   },
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
         id="particle"
         init={particlesInit}
         loaded={particlesLoaded}
-        className="h-[60rem] md:h-[50rem] lg:h-[26rem]"
+        className="h-[80rem] md:h-[50rem] lg:h-[26rem]"
         options={{
           fullScreen: {
             enable: false,
@@ -90,69 +90,73 @@ const Footer = () => {
       />
 
       <div className="absolute  justify-between top-0 w-[100%] text-[#d5d5d5]">
-      <div className="lg:flex px-10  pt-20 space-y-14 lg:space-y-0 ">
-        {/* //first section */}
-        <div className="space-y-4">
-          <img
-            src="https://venovet.com/assets/images/retina-logo2.png"
-            alt=""
-          />
-          <p className="max-w-sm leading-8">
-            Transporting the Goods from Source to our Warehouse, Inwarding the
-            Goods, Quality Check, Storage, Dispatch, Reporting and End Customer
-            Delivery Transportation.
-          </p>
+        <div className="lg:flex px-10  pt-20 space-y-14 lg:space-y-0 ">
+          {/* //first section */}
+          <div className="space-y-4">
+            <img
+              src="https://venovet.com/assets/images/retina-logo2.png"
+              alt=""
+            />
+            <p className="max-w-sm leading-8">
+              Transporting the Goods from Source to our Warehouse, Inwarding the
+              Goods, Quality Check, Storage, Dispatch, Reporting and End
+              Customer Delivery Transportation.
+            </p>
 
-          {/* social icons  */}
-          <div className="flex space-x-4 pt-4">
-            <AiFillFacebook size={30} />
-            <AiOutlineTwitter size={30} />
-            <AiOutlineInstagram size={30}/>
-            <AiFillLinkedin size={30}/>
+            {/* social icons  */}
+            <div className="flex space-x-4 pt-4">
+              <AiFillFacebook size={30} />
+              <AiOutlineTwitter size={30} />
+              <AiOutlineInstagram size={30} />
+              <AiFillLinkedin size={30} />
+            </div>
+          </div>
+          {/* second section  */}
+          <div className="md:flex md:justify-between space-y-8 md:space-y-0 md:space-x-20">
+            <div className="space-y-4">
+              <h1 className="text-white text-lg font-semibold">ABOUT US</h1>
+              <ul className="space-y-4 text-[17px] cursor-pointer">
+                <li>About Us</li>
+                <li>What we do</li>
+                <li>Who we serve</li>
+                <li>Our Blog</li>
+              </ul>
+            </div>
+            <div className="space-y-4">
+              <h1 className="text-white text-lg font-semibold">SUPPORT</h1>
+              <ul className="space-y-4 text-[17px] cursor-pointer">
+                <li>Request a Quote</li>
+                <li>Contac Us</li>
+                <li>FAQ's</li>
+                <li>Careers</li>
+              </ul>
+            </div>
+            <div className="space-y-4">
+              <h1 className="text-white text-lg font-semibold">LINKS</h1>
+              <ul className="space-y-4 text-[17px] cursor-pointer">
+                <li>Shop</li>
+                <li>News</li>
+                <li>Gallery</li>
+                <li>Case Studies</li>
+              </ul>
+            </div>
+          </div>
+          {/* third section  */}
+          <div className="lg:px-10  space-y-10">
+            <div>
+              <p className="text-white">Call Us</p>
+              <p className="text-[#ff5e15] text-2xl font-bold cursor-pointer">
+                +91 9912 742 555
+              </p>
+            </div>
+            <div>
+              <p className="text-white">Mail Us</p>
+              <p className="text-[#1065cd] text-2xl font-bold cursor-pointer">
+                sales@venovet.com
+              </p>
+            </div>
           </div>
         </div>
-        {/* second section  */}
-        <div className="flex justify-between space-x-20">
-          <div className="space-y-4">
-            <h1 className="text-white text-lg font-semibold">ABOUT US</h1>
-            <ul className="space-y-4 text-[17px] cursor-pointer">
-              <li>About Us</li>
-              <li>What we do</li>
-              <li>Who we serve</li>
-              <li>Our Blog</li>
-            </ul>
-          </div>
-          <div className="space-y-4">
-            <h1 className="text-white text-lg font-semibold">SUPPORT</h1>
-            <ul className="space-y-4 text-[17px] cursor-pointer">
-              <li>Request a Quote</li>
-              <li>Contac Us</li>
-              <li>FAQ's</li>
-              <li>Careers</li>
-            </ul>
-          </div>
-          <div className="space-y-4">
-            <h1 className="text-white text-lg font-semibold">LINKS</h1>
-            <ul className="space-y-4 text-[17px] cursor-pointer">
-              <li>Shop</li>
-              <li>News</li>
-              <li>Gallery</li>
-              <li>Case Studies</li>
-            </ul>
-          </div>
-        </div>
-        {/* third section  */}
-        <div className="px-10  space-y-10">
-          <div>
-            <p className="text-white">Call Us</p>
-            <p className="text-[#ff5e15] text-2xl font-bold cursor-pointer">+91 9912 742 555</p>
-          </div>
-          <div>
-            <p className="text-white">Mail Us</p>
-            <p className="text-[#1065cd] text-2xl font-bold cursor-pointer">sales@venovet.com</p>
-          </div>
-        </div>
-      </div>
       </div>
     </div>
   );

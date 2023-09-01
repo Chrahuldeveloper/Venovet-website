05475f43130091980528209185448b98bb3070a5
Rahul <chrahulofficial@gmail.com>
2023-09-02 00:49:55 +0530

    done fetching

--- src/components/Services/Card.js
+++ src/components/Services/Card.js
@@ -1,26 +1,18 @@
 import React from "react";
 
-export default function Card() {
+export default function Card({ data }) {
   const CardData = [
     {
       image: "https://venovet.com/assets/images/icon4.png",
-      Tittle: "Quality & Compliance",
-      Para: "Our warehouses are compliant with CLU & government regulations and adhere to the 5S methodology. We have implemented TQM with centralised monitoring and conduct periodic audits.",
     },
     {
       image: "https://venovet.com/assets/images/icon2.png",
-      Tittle: "Safety & Security",
-      Para: "We aim to provide a safe & ergonomic working environment to maximise ‘Accident-free days’. All our facilities have 24*7 security surveillance across all areas.",
     },
     {
       image: "https://venovet.com/assets/images/icon3.png",
-      Tittle: "Robust Technology",
-      Para: "A dedicated and robust Warehouse Management System (WMS) and Transport Management System (TMS) helps us ensure greater operational efficiency.",
     },
     {
       image: "https://venovet.com/assets/images/icon1.png",
-      Tittle: "World Class Infrastructure",
-      Para: "Our warehouses are equipped with state-of-the-art Material Handling Equipment (MHE), coding machines, racking systems and a host of other best-in-class facilities.",
     },
   ];
 
@@ -32,8 +24,8 @@ export default function Card() {
             <div className="bg-[#fff3e1]  flex items-start max-w-md gap-4 p-5 rounded-lg">
               <img src={_.image} className="w-10 h-10" alt={index} />
               <div className="space-y-2">
-                <h1 className="text-lg font-semibold">{_.Tittle}</h1>
-                <p className="text-[#7a7a7a] lg:text-sm ">{_.Para}</p>
+                <h1 className="text-lg font-semibold">{data.Tittle}</h1>
+                <p className="text-[#7a7a7a] lg:text-sm ">{data.Para}</p>
               </div>
             </div>
           </React.Fragment>
--- src/components/Services/WareHouseManagement.js
+++ src/components/Services/WareHouseManagement.js
@@ -7,21 +7,11 @@ export default function WareHouseManagement({ data }) {
     <section classNamw="mt-5 md:mt-0">
       <div className="space-y-6">
         <h1 className="max-w-sm font-semibold md:max-w-full md:text-2xl lg:text-4xl">
-          Consistent, reliable and efficient warehouse management
+          {data.Tittle1}
         </h1>
         <div className="flex flex-col items-start space-y-10 lg:space-y-0 lg:space-x-10 lg:flex-row text-[#7a7a7a]">
-          <p>
-            Venovet maintains and manages a network of Grade A warehousing
-            facilities strategically located across India. Our warehousing
-            services are trusted by the industry's front-runners for their
-            consistent, reliable and efficient performance. Our experienced team
-            makes use of the dedicated
-          </p>
-          <p>
-            Warehouse Management System (WMS) & Transport Management System
-            (TMS) that provides real-time data intelligence to re-model
-            inventory management, driving measurable cost reductions.
-          </p>
+          <p>{data.Para1}</p>
+          <p>{data.Para2}</p>
         </div>
       </div>
       {/* Offerings section*/}
@@ -30,20 +20,15 @@ export default function WareHouseManagement({ data }) {
         <div className="flex flex-col mt-5 space-y-4 md:items-start lg:space-x-10 lg:flex-row lg:space-y-0">
           <img
             className="max-w-sm rounded-lg"
-            src="https://venovet.com/assets/images/s1.jpg"
+            src={data.SubCat1.image}
             alt="imge"
           />
           <div className="max-w-lg space-y-3.5">
             <h1 className="text-xl font-bold text-orange-500 ">
-              Distribution Warehousing
+              {data.SubCat1.Tittle}
             </h1>
             <p className="text-[#7a7a7a] text-sm leading-6">
-              Use our expertise and warehousing facilities to reduce your
-              inventory level and capital investment by consolidating your small
-              shipments from a number of suppliers in the same geographical area
-              and combine them into larger, more economical shipping loads
-              intended for the same area at a warehouse that act as a singular
-              hub.
+              {data.SubCat1.Para}
             </p>
           </div>
         </div>
@@ -52,20 +37,15 @@ export default function WareHouseManagement({ data }) {
       <div className="flex flex-col items-start space-y-4 lg:flex-row mt-7 lg:space-y-0">
         <img
           className="max-w-sm rounded-lg lg:order-2"
-          src="https://venovet.com/assets/images/s2.jpg"
+          src={data.SubCat2.image}
           alt="imge"
         />
         <div className="max-w-lg space-y-3.5 ">
           <h1 className="text-xl font-bold text-orange-500 ">
-            Distribution Warehousing
+            {data.SubCat2.Tittle}
           </h1>
           <p className="text-[#7a7a7a] text-sm leading-6">
-            Use our expertise and warehousing facilities to reduce your
-            inventory level and capital investment by consolidating your small
-            shipments from a number of suppliers in the same geographical area
-            and combine them into larger, more economical shipping loads
-            intended for the same area at a warehouse that act as a singular
-            hub.
+            {data.SubCat2.Para}
           </p>
         </div>
       </div>
@@ -73,21 +53,15 @@ export default function WareHouseManagement({ data }) {
       <div className="flex flex-col items-start space-y-4 lg:space-x-10 lg:flex-row mt-7 lg:space-y-0">
         <img
           className="max-w-sm rounded-lg"
-          src="https://venovet.com/assets/images/s3.jpg"
+          src={data.SubCat3.image}
           alt="imge"
         />
         <div className="max-w-lg space-y-3.5">
           <h1 className="text-xl font-bold text-orange-500 ">
-            Distribution Warehousing
+            {data.SubCat3.Tittle}
           </h1>
           <p className="text-[#7a7a7a] text-sm leading-6">
-            Leverage our capabilities in developing and managing distribution
-            centres in a dedicated/shared services model focused on driving 100%
-            order fulfilment. We utilise efficient resource management and
-            automation techniques coupled with digitally-enabled purchase order
-            and sales order management. This ensures seamless planning and
-            execution of order fulfillment and provides real time inventory
-            tracking and monitoring.
+            {data.SubCat3.Para}
           </p>
         </div>
       </div>
@@ -95,18 +69,15 @@ export default function WareHouseManagement({ data }) {
       <div className="flex flex-col items-start space-y-4 lg:flex-row mt-7 lg:space-y-0">
         <img
           className="max-w-sm rounded-lg lg:order-2"
-          src="https://venovet.com/assets/images/s4.jpg"
+          src={data.SubCat4.image}
           alt="imge"
         />
         <div className="max-w-lg space-y-3.5">
           <h1 className="text-xl font-bold text-orange-500 ">
-            Distribution Warehousing
+            {data.SubCat4.Tittle}
           </h1>
           <p className="text-[#7a7a7a] text-sm leading-6">
-            Improving efficiencies by expanding capabilities With operational
-            and service excellence as our cornerstones, we offer a bouquet of
-            services that complement our warehousing and transportation
-            capabilities. These include:
+            {data.SubCat4.Para}
             <ul className="pt-5 pl-4">
               <li className="list-disc">Labelling</li>
               <li className="list-disc">Reverse logistics</li>
@@ -122,9 +93,9 @@ export default function WareHouseManagement({ data }) {
       {/* efficiency your competitive advantage section */}
       <div className="mt-7">
         <h1 className="text-xl font-semibold text-center md:text-2xl lg:text-3xl ">
-          Make efficiency your competitive advantage
+          {data.Tittle3}
         </h1>
-        <Card />
+        <Card data={data} />
       </div>
       {/* efficiency your competitive advantage section */}
 
@@ -132,39 +103,21 @@ export default function WareHouseManagement({ data }) {
       <div className="flex flex-col items-start justify-start gap-8 mt-8 lg:flex-row">
         <div className="space-y-3">
           <h1 className="text-lg font-bold md:text-xl lg:text-2xl">
-            Experience Flexible Warehousing
+            {data.Subcat9.Tittle}
           </h1>
-          <h2 className="font-semibold">
-            Are you paying as per fixed terms when your requirements are
-            dynamic?
-          </h2>
+          <h2 className="font-semibold">{data.Subcat9.SubTittle}</h2>
           <p className="text-[#777777] leading-7 text-sm">
-            Forget rigid contracts spanning years and leverage the on-demand
-            services of our Multi User Facilities (MUFs) through a pay-per-use
-            model. Get the unflinching support of a team of specialists,
-            dedicated to helping you reduce costs, enhance customer service and
-            drive efficiencies.
+            {data.Subcat9.Para}
           </p>
         </div>
-        <img
-          className="max-w-sm rounded-lg"
-          src="https://venovet.com/assets/images/s5.jpg"
-          alt=""
-        />
+        <img className="max-w-sm rounded-lg" src={data.Subcat9.image} alt="" />
       </div>
       {/* Experience Flexible Warehousing section */}
-
       {/* why us section */}
       <div className="mt-7">
         <div className="space-y-3">
-          <h1 className="text-lg md:text-xl lg:text-3xl">Why Us!</h1>
-          <p className="text-[#777777] leading-7 text-sm">
-            We continue to pursue that same vision in today’s complex, uncertain
-            world, working every day to earn our customers’ trust! During that
-            time, we’ve become expert in freight transportation by air and all
-            its related services. We work closely with all major airlines around
-            the world.
-          </p>
+          <h1 className="text-lg md:text-xl lg:text-3xl">{data.Tittle5}</h1>
+          <p className="text-[#777777] leading-7 text-sm">{data.Para5}</p>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-6 lg:grid-cols-3">
           <Icons />

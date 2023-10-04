b91190a86539a5f778b61dc06f43d3f18343e5ac
Rahul <chrahulofficial@gmail.com>
2023-10-04 21:26:42 +0530

    done

--- src/components/Services.js
+++ src/components/Services.js
@@ -9,7 +9,7 @@ function Services() {
     <div className="px-10 py-16 pt-24 bg-[#dae2ed] space-y-14">
       <div className="space-y-4">
         <h1 className="text-[#ff5e15] font-semibold lg:ml-20">OUR SERVICES</h1>
-        <h1 className="lg:ml-20 text-3xl font-bold md:text-4xl">What We Do</h1>
+        <h1 className="text-3xl font-bold lg:ml-20 md:text-4xl">What We Do</h1>
       </div>
       <div className="grid py-3 space-y-20 md:space-y-0 md:grid-cols-2 lg:grid-cols-4 md:gap-14 lg:gap-7 place-items-center lg:px-20">
         {Data.map((card, index) => {
@@ -21,11 +21,11 @@ function Services() {
             >
               <div className="overflow-hidden">
                 <img
-                  className={`rounded-lg   ${
+                  className={`rounded-lg  ${
                     hover === index
                       ? "brightness-75 ease-in-out transition duration-300 scale-110 rotate-3"
                       : null
-                  }  cursor-pointer w-96 lg:w-0`}
+                  }  cursor-pointer w-96 `}
                   src={card.image}
                   alt=""
                 />
@@ -64,7 +64,7 @@ function Services() {
           }}
         >
           <button className="bg-[#ff5e15] relative overflow-hidden group  transition ease-in-out duration-300 font-semibold rounded-xl  text-white px-6 py-2 flex items-center space-x-2">
-            <span className="absolute left-0 h-full w-0 transition-all bg-orange-700 opacity-100 group-hover:w-full duration-300 ease-in-out"></span>
+            <span className="absolute left-0 w-0 h-full transition-all duration-300 ease-in-out bg-orange-700 opacity-100 group-hover:w-full"></span>
             <span className="relative flex items-center">
               <h1>View All Services</h1>
               <AiOutlineArrowRight size={25} />

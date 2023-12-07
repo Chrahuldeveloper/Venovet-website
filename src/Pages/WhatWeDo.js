09c9f1f23e9a47851aab14bb983ef8148ed359c8
Rahul <chrahulofficial@gmail.com>
2023-12-07 17:53:29 +0530

    dn

--- src/Pages/WhatWeDo.js
+++ src/Pages/WhatWeDo.js
@@ -1,19 +1,10 @@
 import React from "react";
-import {
-  DragTop,
-  ExpertFrom,
-  FilterCategory,
-  Footer,
-  Navbar,
-  NewsLetter,
-  ServiceHeroSection,
-} from "../components";
+import * as components from "../components";
 import { Help, Brochure, ServiceContent } from "../components/Services/index";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import DropdownData from "../Data/WhatweDoCategories";
 export default function WhatWeDo() {
   // From Params take the categeory and chamge the whole ui
-  const data = useLocation();
   const { category } = useParams();
   const decodedCategory = decodeURIComponent(category);
   const defaultData = DropdownData.find(
@@ -25,26 +16,26 @@ export default function WhatWeDo() {
   return (
     <body className="overflow-x-clip">
       <main>
-        <Navbar Page={"whatwedo"} />
-        <ServiceHeroSection
+        <components.Navbar Page={"whatwedo"} />
+        <components.ServiceHeroSection
           image={defaultData.image}
           Tittle={defaultData.Tittle}
           Para={defaultData.Para}
         />
         <div className="flex flex-col max-w-5xl gap-16 p-5 px-5 mx-auto lg:max-w-none md:px-12 lg:px-24 lg:flex-row md:justify-around md:items-start">
           <div className="space-y-8">
-            <FilterCategory Tittle={"Category"} Page={"whatwedo"} />
+            <components.FilterCategory Tittle={"Category"} Page={"whatwedo"} />
             <Help />
             <Brochure />
-            <ExpertFrom />
+            <components.ExpertFrom />
           </div>
           <div>
             <ServiceContent category={decodedCategory} />
           </div>
         </div>
-        <NewsLetter />
-        <DragTop />
-        <Footer />
+        <components.NewsLetter />
+        <components.DragTop />
+        <components.Footer />
       </main>
     </body>
   );

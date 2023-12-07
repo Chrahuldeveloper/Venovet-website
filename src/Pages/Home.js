e2143fc7f8cd351b99530e3c22bb35a92bedb2c5
unknown <jayprakashpj2525@gmail.com>
2023-12-07 18:00:28 +0530

    done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -19,26 +19,51 @@ import { ColorRing } from "react-loader-spinner";
 
 export default function Home() {
   const location = useLocation();
-  const [isSubmitting, setIsSubmiting] = useState(true);
+  // const [isSubmitting, setIsSubmiting] = useState(true);
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
+  // useEffect(() => {
+  //   window.scrollTo(0, 0);
+  // }, [location]);
+
+  // useEffect(() => {
+  //   const handleLoad = () => {
+  //     setIsSubmiting(false);
+  //   };
+
+  //   window.addEventListener("load", handleLoad);
+  //   //to avoid memory leaks
+  //   return () => {
+  //     window.removeEventListener("load", handleLoad);
+  //   };
+  // }, []);
+  const [isContentLoaded, setIsContentLoaded] = useState(false);
 
   useEffect(() => {
     const handleLoad = () => {
-      setIsSubmiting(false);
+      setIsContentLoaded(true);
     };
 
-    window.addEventListener("load", handleLoad);
-    //to avoid memory leaks
+    // Check if the content is already loaded
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+    }
+
+    // Clean up the event listener when the component unmounts
     return () => {
       window.removeEventListener("load", handleLoad);
     };
   }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    // Reset loading status when the location changes
+    setIsContentLoaded(true);
+  }, [location]);
   return (
     <div className="scroll-smooth overflow-x-clip animate__animated animate__fadeIn animate__delay-0.3s">
-      {isSubmitting && ( // Render loader only when isSubmitting is true
+      {!isContentLoaded && ( // Render loader only when isSubmitting is true
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-100 bg-opacity-75">
           <ColorRing
             visible={true}
